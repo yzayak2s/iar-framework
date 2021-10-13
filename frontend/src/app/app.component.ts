42d1d0ebcf9b76670f11e56ce131767a28611c0f
LUC4R4T0R <luc4r4t0r@gmail.com>
2021-10-13 11:02:27 +0200

    fixed login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "./services/auth.service";
 
 @Component({
@@ -6,8 +6,14 @@ import {AuthService} from "./services/auth.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  constructor(public authService: AuthService) {
+  isLoggedIn: boolean;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.subscribeLoginChange((newState: boolean) => {this.isLoggedIn = newState});
   }
 }
