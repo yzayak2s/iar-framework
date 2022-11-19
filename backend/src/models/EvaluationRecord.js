8267140a7d40190a295235e11ddf7214294ac2f4
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2022-11-19 17:45:27 +0100

    implemented:  - constructor()-method in EvaluationRecord.js
    created:
     - evaluation-record-api.js
     - evaluation-record-service.js
     - EvaluationRecord.js

---
+++ backend/src/models/EvaluationRecord.js
@@ -0,0 +1,31 @@
+/**
+ * this model specifies the format to exchange
+ * 'evaluationRecord-data' with the frontend
+ * and store it in mongoDB
+ * @param {number} _goalID
+ * @param {string} goalDescription
+ * @param {number} targetValue
+ * @param {number} actualValue
+ * @param {number} year
+ * @param {number} salesManID
+ */
+
+class EvaluationRecord {
+    constructor(
+        _goalID,
+        goalDescription,
+        targetValue,
+        actualValue,
+        year,
+        salesManID
+    ) {
+        this._goalID = _goalID;
+        this.goalDescription = goalDescription;
+        this.targetValue = targetValue;
+        this.actualValue = actualValue;
+        this.year = year;
+        this.salesManID= salesManID;
+    }
+}
+
+module.exports = EvaluationRecord;
