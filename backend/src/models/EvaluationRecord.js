dcd88ba28a2fefd1d3200175334b38dbe4d8b25f
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-12-03 20:55:21 +0100

    add schema for evaluation records

--- backend/src/models/EvaluationRecord.js
+++ backend/src/models/EvaluationRecord.js
@@ -1,13 +1,35 @@
 /**
- * this model specifies the format to exchange
- * 'evaluationRecord-data' with the frontend
- * and store it in mongoDB
- * @param {number} _goalID
- * @param {string} goalDescription
- * @param {number} targetValue
- * @param {number} actualValue
- * @param {number} year
- * @param {number} salesManID
+ * @swagger
+ * components:
+ *  schemas:
+ *      Evaluation_Record:
+ *          type: object
+ *          required:
+ *              - goalID
+ *              - goalDescription
+ *              - targetValue
+ *              - actualValue
+ *              - year
+ *              - salesManID
+ *          properties:
+ *              goalID:
+ *                  type: int
+ *                  description: id of the evaluation Record
+ *              goalDescription:
+ *                  type: string
+ *                  description: Description of our goal
+ *              targetValue:
+ *                  type: int
+ *                  description: The amount of money we want our salesman to reach this year
+ *              actualValue:
+ *                  type: string
+ *                  description: The current amount of money our salesman has reached
+ *              year:
+ *                  type: string
+ *                  description: The year this Evaluation Record is from
+ *              salesManID:
+ *                  type: int
+ *                  description: The id of the salesman this record belongs to
  */
 
 class EvaluationRecord {
