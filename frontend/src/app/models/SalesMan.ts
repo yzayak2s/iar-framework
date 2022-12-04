d160159e1cd8f2f9eebb1c823ae51884a96989a7
Nada Oukhai <nada.oukhai@smail.inf.h-brs.de>
2022-12-04 14:19:55 +0100

    Implement Salesman modal

---
+++ frontend/src/app/models/SalesMan.ts
@@ -0,0 +1,10 @@
+/**
+ * this model specifies the format to exchange a user with the backend
+ */
+export class SalesMan{
+    constructor(
+        public _id: string,
+        public firstname: string,
+        public lastname: string
+    ) {  }
+}
