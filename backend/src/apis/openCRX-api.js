99e43f573ba5061eb3f7c15415972199ebf49bd9
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-12-06 20:17:23 +0100

    http head is now global since it is reused

--- backend/src/apis/openCRX-api.js
+++ backend/src/apis/openCRX-api.js
@@ -2,20 +2,23 @@ const openCRXService = require("../services/openCRX-service");
 const https = require("https");
 const httpsAgent = new https.Agent({rejectUnauthorized: false});
 
-exports.getAccounts = (req, res) => {
-    const baseUrl = 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX'
-    const credentials = {
-        username: 'guest',
-        password: 'guest'
-    };
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        },
-        httpsAgent: httpsAgent, // this was missing (for what is this)?
-        auth: credentials,
-    };
 
+// OpenCRX request data
+const baseUrl = 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX'
+const credentials = {
+    username: 'guest',
+    password: 'guest'
+};
+const config = {
+    headers: {
+        'Accept': 'application/json'
+    },
+    httpsAgent: httpsAgent, 
+    auth: credentials,
+};
+
+
+exports.getAccounts = (req, res) => {
     openCRXService.getAllAccounts(baseUrl, config).then((accounts) => {
         res.send(accounts);
     }).catch(_ => {
@@ -24,19 +27,6 @@ exports.getAccounts = (req, res) => {
 }
 
 exports.getAccountByUID = (req, res) => {
-    const baseUrl = 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX'
-    const credentials = {
-        username: 'guest',
-        password: 'guest'
-    };
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        },
-        httpsAgent: httpsAgent, // this was missing (for what is this)?
-        auth: credentials,
-    };
-
     openCRXService.getAccountByUID(baseUrl, config, req.params.uid)
         .then((accountByUID) => {
             res.send(accountByUID);
@@ -46,19 +36,6 @@ exports.getAccountByUID = (req, res) => {
 }
 
 exports.getProducts = (req, res) => {
-    const baseUrl = 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX'
-    const credentials = {
-        username: 'guest',
-        password: 'guest'
-    };
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        },
-        httpsAgent: httpsAgent, // this was missing (for what is this)?
-        auth: credentials,
-    };
-
     openCRXService.getAllProducts(baseUrl, config).then((products) => {
         res.send(products);
     }).catch(_ => {
@@ -67,19 +44,6 @@ exports.getProducts = (req, res) => {
 }
 
 exports.getProductByUID = (req, res) => {
-    const baseUrl = 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX'
-    const credentials = {
-        username: 'guest',
-        password: 'guest'
-    };
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        },
-        httpsAgent: httpsAgent, // this was missing (for what is this)?
-        auth: credentials,
-    };
-
     openCRXService.getProductByUID(baseUrl, config, req.params.uid)
         .then((productByUID) => {
             res.send(productByUID);
@@ -90,19 +54,6 @@ exports.getProductByUID = (req, res) => {
 }
 
 exports.getSalesOrders = (req, res) => {
-    const baseUrl = 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX'
-    const credentials = {
-        username: 'guest',
-        password: 'guest'
-    };
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        },
-        httpsAgent: httpsAgent, // this was missing (for what is this)?
-        auth: credentials,
-    };
-
     openCRXService.getAllSalesOrders(baseUrl, config).then(salesOrders => {
         res.send(salesOrders);
     }).catch(_ => {
@@ -111,19 +62,6 @@ exports.getSalesOrders = (req, res) => {
 }
 
 exports.getSalesOrderByUID = (req, res) => {
-    const baseUrl = 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX'
-    const credentials = {
-        username: 'guest',
-        password: 'guest'
-    };
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        },
-        httpsAgent: httpsAgent, // this was missing (for what is this)?
-        auth: credentials,
-    };
-
     openCRXService.getSalesOrderByUID(baseUrl, config, req.params.uid)
         .then((salesOrderByUID) => {
             res.send(salesOrderByUID);
@@ -133,18 +71,6 @@ exports.getSalesOrderByUID = (req, res) => {
 }
 
 exports.getPositions = (req, res) => {
-    const baseUrl = 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX'
-    const credentials = {
-        username: 'guest',
-        password: 'guest'
-    };
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        },
-        httpsAgent: httpsAgent, // this was missing (for what is this)?
-        auth: credentials,
-    };
 
     //openCRXService...
 
