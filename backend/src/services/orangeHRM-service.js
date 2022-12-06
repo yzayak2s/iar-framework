444dce8a8a2d323638ded21c5d169089b62993e9
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2022-12-06 13:17:03 +0100

    implemented getAllEmployees()-method

---
+++ backend/src/services/orangeHRM-service.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+/**
+ * retrieves employees from orangeHRM
+ */
+exports.getAllEmployees = async (baseUrl, body, config) => {
+    const response = await axios.post(
+        `${baseUrl}/oauth/issueToken`,
+        body,
+        config
+    );
+
+    const accessToken = response.data['access_token']; // also with .access_token accessible
+
+    const {httpsAgent} = config
+    const configWithToken = {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json'
+        },
+        httpsAgent: httpsAgent, // this was missing (for what is this)?
+    }
+
+    const employees = await axios.get(`${baseUrl}/api/v1/employee/search`, configWithToken);
+
+    return employees.data.data;
+    /*console.log(accessToken)
+    console.log(expiresAt)
+    console.log(response.data.access_token)
+    console.log(new Date().getTime())
+    console.log(new Date().getTime() + 3600 * 60)*/
+}
