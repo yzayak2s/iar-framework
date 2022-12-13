bdbfe705c11b6fa51107081884e962f1ca4ad84a
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2022-12-13 12:21:30 +0100

    provided getBonuses()-, getBonusById()-, addBonus()-, updateBonusById()- and deleteBonus()-method

---
+++ backend/src/apis/bonus-api.js
@@ -0,0 +1,61 @@
+const bonusService = require("../services/bonus-service");
+
+exports.getBonuses = (req, res) => {
+    const db = req.app.get('db');
+
+    bonusService.getAll(db).then((bonuses) => {
+        res.send(bonuses);
+    }).catch(_ => {
+        res.status(500).send();
+    });
+}
+
+exports.getBonusById = (req, res) => {
+    const db = req.app.get('db');
+
+    bonusService.getBonusById(db, req.params._id)
+        .then((bonusById) => {
+            res.send(bonusById);
+        }).catch(_ => {
+            res.status(500).send();
+    });
+}
+
+exports.addBonus = (req, res) => {
+    const db = req.app.get('db');
+
+    bonusService.add(db, req.body)
+        .then((_id) => {
+            res.send(_id.toString());
+        }).catch((e) => {
+            res.send(e.message);
+    }).catch(_ => {
+        res.status(500).send();
+    });
+}
+
+exports.updateBonusById = (req, res) => {
+    const db = req.app.get('db');
+
+    bonusService.update(db, req.params._id, req.body)
+        .then((bonus) => {
+            res.send(bonus);
+        }).catch((e) => {
+            res.send(e.message);
+    }).catch(_ => {
+        res.status(500).send()
+    });
+}
+
+exports.deleteBonus = (req, res) => {
+    const db = req.app.get('db');
+
+    bonusService.delete(db, req.params._id)
+        .then(_id => {
+            res.send(_id.toString());
+        }).catch((e) => {
+            res.send(e.message);
+    }).catch(_ => {
+        res.status(500).send();
+    });
+}
