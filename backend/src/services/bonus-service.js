0dd21e4af23625ac32bc71b99445e49f2338893a
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2022-12-13 09:05:49 +0100

    implemented add-method for a bonus

--- backend/src/services/bonus-service.js
+++ backend/src/services/bonus-service.js
@@ -6,3 +6,17 @@
 exports.getAll = async (db) => {
     return await db.collection('bonus').find({}).toArray(); // use of toArray() is important here
 }
+
+/**
+ * insert a new bonus into database
+ */
+exports.add = async (db, bonus) => {
+    const existingBonusById = await db.collection('bonus').findOne({_id: bonus._id});
+
+    if (existingBonusById) {
+        if (existingBonusById._id === bonus._id) {
+            throw new Error('Bonus with id ' + bonus._id + ' already exist!');
+        }
+    }
+    return (await db.collection('bonus').insertOne(bonus)).insertedId;
+}
