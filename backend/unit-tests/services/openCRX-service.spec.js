5a071976dce767c28eaa2bf91f74ce5dda68fdda
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-12-14 05:08:57 +0100

    added service

--- backend/unit-tests/services/openCRX-service.spec.js
+++ backend/unit-tests/services/openCRX-service.spec.js
@@ -2,3 +2,5 @@ const chai = require('chai');
 const expect = chai.expect;
 chai.use(require("chai-exclude"));
 chai.use(require('chai-as-promised'));
+
+const openCRXService = require('../../src/services/openCRX-service');
