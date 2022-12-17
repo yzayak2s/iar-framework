81928daa6937c05b11fe94abddd6b0248605ffe5
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-12-17 03:52:39 +0100

    Added environment file for api secrets

---
+++ backend/environments/apiEnvironment.js
@@ -0,0 +1,15 @@
+
+    exports.orangeHRMConfig = {
+        baseUrl: 'https://sepp-hrm.inf.h-brs.de/symfony/web/index.php',
+        username: 'zayakh',
+        password: '*Safb02da42Demo$'
+    };
+
+    exports.openCRXConfig = {
+        baseUrl: 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX',
+        credentials: {
+            username: 'guest',
+            password: 'guest'
+        }
+    };
+
