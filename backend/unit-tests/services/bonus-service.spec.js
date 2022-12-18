6eb831b1a6f141711f2712416b83da009a4ecde6
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-12-18 19:14:41 +0100

    bonus service stuff

--- backend/unit-tests/services/bonus-service.spec.js
+++ backend/unit-tests/services/bonus-service.spec.js
@@ -4,53 +4,46 @@ chai.use(require("chai-exclude"));
 chai.use(require('chai-as-promised'));
 
 const {initMockedMongoDB, resetMockedMongoDB, closeMockedMongoDB} = require('../support/mongodb-mocking');
-const {copyObject} = require('../support/copyObject')
 
 const bonusService = require('../../src/services/bonus-service');
-const bonus = require('../../src/models/Bonus');
-const salesmanService = require('../../src/services/salesman-service');
-const salesman = require('../../src/models/SalesMan');
-const SalesMan = require('../../src/models/SalesMan');
 const Bonus = require('../../src/models/Bonus');
+const salesmanService = require('../../src/services/salesman-service');
+const SalesMan = require('../../src/models/SalesMan');
+const {copyObject} = require('../support/copyObject');
 
 const bonusExample1 = new Bonus(0, 2020, 2000, false, 0);
 const bonusExample2 = new Bonus(1, 2021, 2555, true, 0);
 const bonusExample3 = new Bonus(2, 2021, 1555, false, 1);
 
-describe.only('bonus-service unit-tests', function() {
-    let db;
+const salesmanExample1 = new SalesMan('Max', 'Mustermann', 0);
+const salesmanExample2 = new SalesMan('John', 'Johny', 1)
 
+let db;
+
+describe("bonus-service Unit-tests", function() {
     before(async () => {
         db = await initMockedMongoDB();
+    });
+
+    afterEach(() => {
+        resetMockedMongoDB();
     });
 
     after(() => {
         closeMockedMongoDB();
     });
 
-    afterEach(() => {
-        resetMockedMongoDB(db);
-    });
 
-    beforeEach(() => {
-        salesmanService.add(db, new SalesMan('Max', 'Mustermann', 0));
-        salesmanService.add(db, new SalesMan('John', 'Johny', 1));
-    });
-
-    describe("get All Bonus", async function() {
-        await bonusService.add(db, copyObject(bonusExample1));
-        await bonusService.add(db, copyObject(bonusExample2));
-        await bonusService.add(db, copyObject(bonusExample3));
-
-        const response = bonusService.getAll();
-
-        it("Is an array with length 3", async function() {
-            await expect(response).to.eventually.be.an('array').lengthOf(3);
+    describe.only("Able to add bonuses", function() {
+        beforeEach(async () => {
+            await salesmanService.add(db, copyObject(salesmanExample1));
+            await salesmanService.add(db, copyObject(salesmanExample2));
         });
 
-        it("Got the correct bonuses", async function() {
-            await expect(response).to.eventually.have.members([bonusExample1, bonusExample2, bonusExample3]);
-        });
+        it("Created three bonus", async function() {
+            await expect(salesmanService.getAll(db)).to.eventually.be.an('array').with.lengthOf(2);
 
+            await expect(bonusService.getAll(db)).to.eventually.be.an('array').lengthOf(0)
+        });
     });
 });
