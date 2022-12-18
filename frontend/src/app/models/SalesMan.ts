f77fb61e6a85825fe2256be617ea4c03e00904a5
Nada Oukhai <nada.oukhai@smail.inf.h-brs.de>
2022-12-18 01:26:20 +0100

    change firstname,lastname

--- frontend/src/app/models/SalesMan.ts
+++ frontend/src/app/models/SalesMan.ts
@@ -2,9 +2,8 @@
  * this model specifies the format to exchange a user with the backend
  */
 export class SalesMan{
-    constructor(
-        public _id: string,
-        public firstname: string,
-        public lastname: string
-    ) {  }
+    public _id: string;
+    public firstName: string;
+    public lastName: string;
+
 }
