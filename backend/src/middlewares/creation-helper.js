bed21d1229f48e52976aaae9911ee2fbc9a5774f
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-12-26 23:23:33 +0100

    add helper to compare body and classes

---
+++ backend/src/middlewares/creation-helper.js
@@ -0,0 +1,16 @@
+exports.fitsModel = async (testObject, classModel) => {
+    const testObjectKeys = Object.keys(testObject);
+    const modelKeys = Object.keys(new classModel())
+    
+    // Can't be equal if not same length
+    if (testObjectKeys.length != modelKeys.length) {
+        return false;
+    }
+
+    // Sort and then convert to String to compare
+    if (testObjectKeys.sort().toString() != modelKeys.sort().toString()) {
+        return false;
+    }
+
+    return true;
+}
