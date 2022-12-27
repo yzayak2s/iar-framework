cbe51bb832e4430558f9ad85b388f3ebc3c08db7
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2022-12-27 10:48:45 +0100

    imported Bonus-Interface, Observable and HttpClient and implemented getBonuses()-method

--- frontend/src/app/services/bonus.service.ts
+++ frontend/src/app/services/bonus.service.ts
@@ -1,10 +1,20 @@
 import {Injectable} from '@angular/core';
+import {Bonus} from '../models/Bonus';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class BonusService {
+    private bonusesUrl = 'api/bonuses'; // URL to web api
 
-    constructor() {
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    /** GET bonuses from the server */
+    getBonuses(): Observable<Bonus[]> {
+        return this.http.get<Bonus[]>(this.bonusesUrl);
     }
 }
