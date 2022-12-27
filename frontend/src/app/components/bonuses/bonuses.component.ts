aeaac66eb386e56f8dae8a88f1005806f45f3dd5
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2022-12-27 12:08:46 +0100

    implemented getBonuses()-method and provided corresponding template

--- frontend/src/app/components/bonuses/bonuses.component.ts
+++ frontend/src/app/components/bonuses/bonuses.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Bonus} from '../../interfaces/Bonus';
+import {BonusService} from '../../services/bonus.service';
 
 @Component({
     selector: 'app-bonuses',
@@ -6,10 +8,20 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./bonuses.component.css']
 })
 export class BonusesComponent implements OnInit {
+    bonuses: Bonus[] = [];
 
     constructor(
+        private bonusService: BonusService
     ) { }
 
     ngOnInit(): void {
+        this.getBonuses();
+    }
+
+    getBonuses(): void {
+        this.bonusService.getBonuses()
+            .subscribe(bonuses => {
+                this.bonuses = bonuses;
+            });
     }
 }
