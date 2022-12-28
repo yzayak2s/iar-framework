50a307258a0dd8990f441fb42d04fd0cc68b53e1
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-12-28 05:15:20 +0100

    id now is parsed to int incase of string

--- backend/src/models/SalesMan.js
+++ backend/src/models/SalesMan.js
@@ -28,7 +28,7 @@ class SalesMan {
     constructor(firstname, lastname, _id, uid) {
         this.firstname = firstname;
         this.lastname = lastname;
-        this._id = _id;
+        this._id = parseInt(_id);
         this.uid = uid;
     }
 }
