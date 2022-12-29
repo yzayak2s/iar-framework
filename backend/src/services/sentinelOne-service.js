b9dbf6e26b5d5e13b7f9a4098e2dc06beaa42e15
yzayak2s <younes.zayakh@smail.inf.h-brs.de>
2022-12-29 16:00:32 +0100

    implemented getAgentsStartsWithComputerName()-method

--- backend/src/services/sentinelOne-service.js
+++ backend/src/services/sentinelOne-service.js
@@ -11,3 +11,59 @@ exports.getAgentByComputerName = async (baseUrl, config, computerName) => {
 
     return agentByComputerName.data;
 }
+
+/**
+ * retrieves agents starts with computerName
+ */
+exports.getAgentsStartsWithComputerName = async (baseUrl, config, computerName) => {
+    const agentsStartsWithComputerName = await axios.get(
+        `${baseUrl}/agents?computerName__contains=${computerName}`,
+        config
+    );
+    
+    const agents = agentsStartsWithComputerName.data.data;
+
+    var agentsArray = [];
+    
+    agents.map((value) => {
+        const {
+            computerName,
+            lastActiveDate,
+            encryptedApplications,
+            lastLoggedInUserName,
+            agentVersion,
+            createdAt,
+            osStartTime,
+            totalMemory,
+            externalIp,
+            lastIpToMgmt,
+            serialNumber,
+            modelName,
+            siteName,
+            groupName,
+            operationalState,
+            networkInterfaces,
+        } = value;
+
+        agentsArray.push({
+            computerName,
+            lastActiveDate,
+            encryptedApplications,
+            lastLoggedInUserName,
+            agentVersion,
+            createdAt,
+            osStartTime,
+            totalMemory,
+            externalIp,
+            lastIpToMgmt,
+            serialNumber,
+            modelName,
+            siteName,
+            groupName,
+            operationalState,
+            networkInterfaces,   
+        })
+    });
+
+    return agentsArray;
+}
