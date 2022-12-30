b79a24802d87c7919903e19f651da25163ef3082
Nada Oukhai <nada.oukhai@smail.inf.h-brs.de>
2022-12-31 00:54:42 +0100

    add bonus Model

---
+++ frontend/src/app/models/Bonus.ts
@@ -0,0 +1,13 @@
+/**
+ * this model specifies the format to exchange an evaluation record
+ */
+export class Bonus {
+    public _id: string;
+    public year: number;
+    public value: number;
+    public verified: string;
+    public salesManID: string;
+    /*constructor(
+
+    ) {  }*/
+}
