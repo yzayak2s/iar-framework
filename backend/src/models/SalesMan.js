57022e1aab259aaadc7a17549cfb8509a3929671
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2023-01-01 09:55:39 +0100

    added further property's to salesman model

--- backend/src/models/SalesMan.js
+++ backend/src/models/SalesMan.js
@@ -8,6 +8,10 @@
  *              - id
  *              - firstname
  *              - lastname
+ *              - middleName
+ *              - fullName
+ *              - unit
+ *              - jobTitle
  *          properties:
  *              id:
  *                  type: int
@@ -18,16 +22,44 @@
  *              lastname:
  *                  type: string
  *                  description: The last name of the salesman
+ *              middleName:
+ *                  type: string
+ *                  description: The middle name of the salesman
+ *              fullName:
+ *                  type: string
+ *                  description: The full name of the salesman
+ *              unit:
+ *                  type: string
+ *                  description: The unit of the salesman
+ *              jobTitle:
+ *                  type: string
+ *                  description: The job title of the salesman
  *          example:
  *              id: 5
  *              firstname: Max
  *              lastname: Mustermann
+ *              middleName: Muster
+ *              fullName: Max Mustermann
+ *              unit: Sales
+ *              jobTitle: Senior Salesman
  */
-
 class SalesMan {
-    constructor(firstname, lastname, _id, uid) {
+    constructor(
+        firstname,
+        middleName,
+        lastname,
+        fullName,
+        unit,
+        jobTitle,
+        _id,
+        uid
+    ) {
         this.firstname = firstname;
+        this.middleName = middleName;
         this.lastname = lastname;
+        this.fullName = fullName;
+        this.unit = unit;
+        this.jobTitle = jobTitle
         this._id = parseInt(_id);
         this.uid = uid;
     }
