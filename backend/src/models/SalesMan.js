2248f739ebb7db3dd4794dfb6be79ad7d9027e32
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2023-01-08 01:25:54 +0100

    Revert "added further property's to salesman model"
    This reverts commit 57022e1aab259aaadc7a17549cfb8509a3929671.

--- backend/src/models/SalesMan.js
+++ backend/src/models/SalesMan.js
@@ -1,20 +1,7 @@
 class SalesMan {
-    constructor(
-        firstname,
-        middleName,
-        lastname,
-        fullName,
-        unit,
-        jobTitle,
-        _id,
-        uid
-    ) {
+    constructor(firstname, lastname, _id, uid) {
         this.firstname = firstname;
-        this.middleName = middleName;
         this.lastname = lastname;
-        this.fullName = fullName;
-        this.unit = unit;
-        this.jobTitle = jobTitle
         this._id = parseInt(_id);
         this.uid = uid;
     }
