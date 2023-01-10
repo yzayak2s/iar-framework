f46a89ade425646197e4f9e1da00971ec55a39ae
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2023-01-10 10:59:19 +0100

    implemented getSalesMan()-method in SalesManComponent

--- frontend/src/app/components/salesman/salesman.component.ts
+++ frontend/src/app/components/salesman/salesman.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {SalesMan} from '../../models/SalesMan';
+import {SalesManService} from '../../services/sales-man.service';
+import {ActivatedRoute} from '@angular/router';
+import {BonusService} from '../../services/bonus.service';
+import {Bonus} from '../../models/Bonus';
 
 @Component({
     selector: 'app-salesman',
@@ -6,11 +11,30 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./salesman.component.css']
 })
 export class SalesmanComponent implements OnInit {
+    salesman: SalesMan;
+    bonus: Bonus;
 
-    constructor() {
+    constructor(
+        private route: ActivatedRoute,
+        private salesManService: SalesManService,
+        private bonusService: BonusService
+    ) {
     }
 
     ngOnInit(): void {
+        this.getSalesMan();
+    }
+
+    getSalesMan(): void {
+        const id = this.route.snapshot.paramMap.get('id');
+        this.bonusService.getBonus(id)
+            .subscribe((bonus): void => {
+                this.bonus = bonus.body;
+                this.salesManService.getSalesMan(bonus.body.salesManID)
+                    .subscribe((salesMan): void => {
+                        this.salesman = salesMan.body;
+                    });
+            });
     }
 
 }
