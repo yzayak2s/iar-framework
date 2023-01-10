84a51a4cbb9c47bb396026baaa60ed1ee6cc66ec
Nada Oukhai <nada.oukhai@smail.inf.h-brs.de>
2023-01-10 15:13:50 +0100

    class Goal implemented

--- frontend/src/app/models/EvaluationRecord.ts
+++ frontend/src/app/models/EvaluationRecord.ts
@@ -1,6 +1,8 @@
 /**
  * this model specifies the format to exchange an evaluation record
  */
+import {SalesMan} from "./SalesMan";
+
 export class EvaluationRecord{
     public _id: string;
     public goalDescription: string;
@@ -8,7 +10,18 @@ export class EvaluationRecord{
     public actualValue: string;
     public year: string;
     public salesManID: string;
+    public salesMan: SalesMan;
     /*constructor(
 
     ) {  }*/
+}
+
+export class Goal {
+    public goal_id: number
+    public goal_description : string
+
+    constructor(goal_id, goal_description) {
+        this.goal_id = goal_id
+        this.goal_description = goal_description
+    }
 }
