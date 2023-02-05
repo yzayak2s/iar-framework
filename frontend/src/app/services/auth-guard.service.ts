b89a26993dd7666525383e920e40143d60f4c4ee
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2023-02-05 17:13:08 +0100

    Add check for User role

--- frontend/src/app/services/auth-guard.service.ts
+++ frontend/src/app/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {CanActivate, Router} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router} from '@angular/router';
 import {AuthService} from './auth.service';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
@@ -15,7 +15,7 @@ export class AuthGuardService implements CanActivate{
 
     constructor(private authService: AuthService, private router: Router) { }
 
-    canActivate(): Observable<boolean> {
+    canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     // mapping isLoggedIn():Observable to this function:
         return this.authService.isLoggedIn()
             .pipe(
@@ -23,6 +23,22 @@ export class AuthGuardService implements CanActivate{
                     if (!state) { // go back to login, if user is not allowed to enter
                         void this.router.navigate(['login']);
                     }
+
+                    // Check if role is allowed
+                    const userRole = this.authService.getRole();
+
+                    // Admin always allowed
+                    if (userRole === 'ADMIN') {
+                        return state;
+                    }
+
+                    const allowedRoles: Array<string> = route.data.roles;
+
+                    if(!allowedRoles.includes(userRole)){
+                        console.log("Not allowed")
+                        void this.router.navigate(['login']);   // If not allowed back to login. Dashboard would create infinite loop here
+                    }
+
                     return state;
                 })
             );
