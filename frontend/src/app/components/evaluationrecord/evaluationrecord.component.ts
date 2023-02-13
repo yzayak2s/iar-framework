9c76884005b61f302ef73eb694251310e26c9c7e
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2023-02-13 02:03:57 +0100

    removed filterBonus()-method and evaluationRecordService (no needed anymore)

--- frontend/src/app/components/evaluationrecord/evaluationrecord.component.ts
+++ frontend/src/app/components/evaluationrecord/evaluationrecord.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {EvaluationRecord} from '../../models/EvaluationRecord';
 import {Bonus} from '../../models/Bonus';
 import {ActivatedRoute} from '@angular/router';
-import {EvaluationRecordService} from '../../services/evaluation-record.service';
 import {BonusService} from '../../services/bonus.service';
 import {BonusComputationService} from '../../services/bonus-computation.service';
 
@@ -12,14 +10,12 @@ import {BonusComputationService} from '../../services/bonus-computation.service'
     styleUrls: ['./evaluationrecord.component.css']
 })
 export class EvaluationrecordComponent implements OnInit {
-    evaluationRecords: EvaluationRecord[];
     bonus: Bonus;
     bonusComputation: any;
     displayedColumns = ['goalDescription', 'targetValue', 'actualValue', 'bonus'];
 
     constructor(
         private route: ActivatedRoute,
-        private evaluationRecordService: EvaluationRecordService,
         private bonusService: BonusService,
         private bonusComputationService: BonusComputationService
     ) {
@@ -34,18 +30,11 @@ export class EvaluationrecordComponent implements OnInit {
         this.bonusService.getBonus(id)
             .subscribe((bonus): void => {
                 this.bonus = bonus.body;
-                this.evaluationRecordService.getEvaluationRecordsBySalesManIDAndYear(this.bonus.salesManID, this.bonus.year)
-                    .subscribe((evaluationRecords): void => {
-                        this.evaluationRecords = evaluationRecords.body
-                    })
                 this.bonusComputationService.getBonusComputationBySalesManIDAndYear(this.bonus.salesManID, this.bonus.year)
                     .subscribe((bonusComputation) => {
                         this.bonusComputation = bonusComputation.body
+                        console.log(this.bonusComputation);
                     })
             })
-    }
-
-    filterBonus(evaluationRecord: any, bonusComputation: any): number {
-        return bonusComputation.perfBonus.evalRecords.filter((value) => value.goalDescription === evaluationRecord.goalDescription)[0].bonus
     }
 }
