8b3d8a5fe0a1975b20bdf298783b02a6e6b028fc
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2023-02-14 18:16:47 +0100

    implemented rejectBonus()-method in BonusDetailComponent

--- frontend/src/app/components/bonus-detail/bonus-detail.component.ts
+++ frontend/src/app/components/bonus-detail/bonus-detail.component.ts
@@ -88,8 +88,47 @@ export class BonusDetailComponent implements OnInit {
     }
 
     rejectBonus(): void {
-        if (confirm('You are going to reject this bonus.\nAre you sure?')) {
+        if (this.bonus.remark === '') {
+        } else if (confirm('You are going to reject this bonus.\nAre you sure?')) {
+            switch (this.bonus.verified.toUpperCase()) {
+            case 'CALCULATED':
+                this.bonus.verified = 'REJECTEDCEO';
+                break;
+            case 'APPROVEDCEO':
+                this.bonus.verified = 'REJECTEDHR';
+                break;
+            case 'APPROVEDHR':
+                this.bonus.verified = 'REJECTED';
+                break;
+            default:
+                break;
+            }
+
             console.log('Rejected!');
+
+            if (this.bonus) {
+                switch (this.user.role) {
+                case 'CEO':
+                    this.bonusService.updateBonusRemark(this.bonus)
+                        .subscribe();
+                    this.bonusService.updateBonusStatus(this.bonus)
+                        .subscribe((): void => this.goBack());
+                    break;
+                case 'HR':
+                    this.bonusService.updateBonusStatus(this.bonus)
+                        .subscribe((): void => this.goBack());
+                    break;
+                case 'SALESMAN':
+                    this.bonusService.updateBonusStatus(this.bonus)
+                        .subscribe((): void => this.goBack());
+                    this.bonusService.createBonusInOrangeHRM(this.bonus)
+                        .subscribe();
+                    break;
+                default:
+                    break;
+
+                }
+            }
             /* if (this.bonus) {
                     this.bonusService.updateBonus(this.bonus)
                         .subscribe((): void => this.goBack());
