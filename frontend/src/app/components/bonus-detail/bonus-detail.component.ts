3418c0f857a444a6f899f365aedb983a10171e99
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2023-02-14 11:28:53 +0100

    implemented approveBonus()- and rejectBonus()-method

--- frontend/src/app/components/bonus-detail/bonus-detail.component.ts
+++ frontend/src/app/components/bonus-detail/bonus-detail.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute} from '@angular/router';
 import {BonusService} from '../../services/bonus.service';
 import {Location} from '@angular/common';
 import {UserService} from '../../services/user.service';
+import {User} from '../../models/User';
 
 @Component({
     selector: 'app-bonus-detail',
@@ -12,6 +13,7 @@ import {UserService} from '../../services/user.service';
 })
 export class BonusDetailComponent implements OnInit {
     bonus: Bonus | undefined;
+    user: User;
     allowedWrite = false;
 
     constructor(
@@ -19,11 +21,13 @@ export class BonusDetailComponent implements OnInit {
         private bonusService: BonusService,
         private location: Location,
         private userService: UserService
-    ) { }
+    ) {
+    }
 
     ngOnInit(): void {
         this.getBonus();
         this.userService.getOwnUser().subscribe((user): void => {
+            this.user = user;
             if (user.role === 'CEO' || user.isAdmin) {
                 this.allowedWrite = true;
             }
@@ -40,10 +44,46 @@ export class BonusDetailComponent implements OnInit {
         this.location.back();
     }
 
-    save(): void {
+    approveBonus(): void {
+        switch (this.bonus.verified.toUpperCase()) {
+        case 'CALCULATED':
+            this.bonus.verified = 'APPROVEDCEO'; break;
+        case 'APPROVEDCEO':
+            this.bonus.verified = 'APPROVEDHR'; break;
+        case 'APPROVEDHR':
+            this.bonus.verified = 'ACCEPTED'; break;
+        default: break;
+        }
+
+        console.log(this.bonus);
+
         if (this.bonus) {
+            switch (this.user.role) {
+            case 'CEO':
+                this.bonusService.updateBonusRemark(this.bonus)
+                    .subscribe();
+                this.bonusService.updateBonusStatus(this.bonus)
+                    .subscribe(/*(): void => this.goBack()*/);
+                break;
+            case 'HR':
+                this.bonusService.updateBonusStatus(this.bonus)
+                    .subscribe(/*(): void => this.goBack()*/);
+                break;
+            case 'SALESMAN':
+                this.bonusService.updateBonusStatus(this.bonus)
+                    .subscribe(/*(): void => this.goBack()*/);
+                break;
+            default: break;
+
+            }
+        }
+    }
+
+    rejectBonus(): void {
+        console.log('Rejected!');
+        /*if (this.bonus) {
             this.bonusService.updateBonus(this.bonus)
                 .subscribe((): void => this.goBack());
-        }
+        }*/
     }
 }
